e661e95ff47e866beede0380f48a481938104ed5
Paul Reinholdtsen <paul@bitsy.services>
2025-12-30 13:40:36 -0800

    other quotes

--- src/Liquid.sol
+++ src/Liquid.sol
@@ -66,14 +66,14 @@ contract Liquid is ERC20, ReentrancyGuardTransient {
         water = buyQuote(liquids, pool, lake);
     }
 
-    function buyQuote(uint256 liquids, Liquid other) public view returns (uint256 others) {
-        uint256 water = buyQuote(liquids);
-        others = other.buyWithQuote(water);
-    }
-
     function sellQuote(uint256 liquids) public view returns (uint256 water) {
         (uint256 pool, uint256 lake) = balances();
         water = sellQuote(liquids, pool, lake);
+    }
+
+    function buyQuote(uint256 liquids, Liquid other) public view returns (uint256 others) {
+        uint256 water = buyQuote(liquids);
+        others = other.buyWithQuote(water);
     }
 
     function sellQuote(uint256 liquids, Liquid other) public view returns (uint256 others) {
@@ -86,14 +86,19 @@ contract Liquid is ERC20, ReentrancyGuardTransient {
         liquids = sellQuote(water, lake, pool);
     }
 
-    function buyWithQuote(uint256 pour, Liquid wine) public view returns (uint256 liquids) {
-        uint256 water = sellQuote(liquids);
-        pour = wine.sellForQuote(water);
+    function buyWithQuote(uint256 others, Liquid other) public view returns (uint256 liquids) {
+        uint256 water = buyWithQuote(others);
+        liquids = other.buyWithQuote(water);
     }
 
     function sellForQuote(uint256 water) public view returns (uint256 liquids) {
         (uint256 pool, uint256 lake) = balances();
         water = buyQuote(liquids, lake, pool);
+    }
+
+    function sellForQuote(uint256 others, Liquid other) public view returns (uint256 liquids) {
+        uint256 water = sellForQuote(others);
+        liquids = other.sellForQuote(water);
     }
 
     function buy(uint256 liquids) external returns (uint256 water) {
