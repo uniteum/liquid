c50627a2c012e68d1785f999a487b32f6145565a
Paul Reinholdtsen <paul@bitsy.services>
2025-12-30 19:06:07 -0800

    simplify giveaway

--- test/Liquid.t.sol
+++ test/Liquid.t.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.30;
 import {Liquid} from "../src/Liquid.sol";
 import {BaseTest, console} from "./Base.t.sol";
 import {TestToken} from "./TestToken.sol";
-import {LiquidUser} from "./LiquidUser.sol";
+import {LiquidUser, IERC20} from "./LiquidUser.sol";
 
 contract LiquidTest is BaseTest {
     Liquid public immutable WATER = new Liquid();
@@ -37,13 +37,17 @@ contract LiquidTest is BaseTest {
         user = new LiquidUser(name, WATER);
     }
 
+    function give(LiquidUser user, uint256 amount, IERC20 token) internal {
+        owen.give(address(user), amount, token);
+    }
+
     function giveaway() internal {
-        owen.give(address(alex), 1e3, WATER);
-        owen.give(address(alex), 1e3, U.solid());
-        owen.give(address(alex), 1e3, V.solid());
-        owen.give(address(beck), 1e7, WATER);
-        owen.give(address(beck), 1e3, U.solid());
-        owen.give(address(beck), 1e3, V.solid());
+        give(alex, 1e3, WATER);
+        give(alex, 1e3, U.solid());
+        give(alex, 1e3, V.solid());
+        give(beck, 1e7, WATER);
+        give(beck, 1e3, U.solid());
+        give(beck, 1e3, V.solid());
     }
 
     function test_Pool() public returns (uint256 liquids, uint256 solids) {
