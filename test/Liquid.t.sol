83af32a7e960fbd377226f86057af8bb79a7d7af
Paul Reinholdtsen <paul@bitsy.services>
2025-12-30 18:11:46 -0800

    one to WATER

--- test/Liquid.t.sol
+++ test/Liquid.t.sol
@@ -8,7 +8,7 @@ import {TestToken} from "./TestToken.sol";
 import {LiquidUser} from "./LiquidUser.sol";
 
 contract LiquidTest is BaseTest {
-    Liquid one = new Liquid();
+    Liquid public immutable WATER = new Liquid();
     Liquid public U;
     Liquid public V;
     TestToken public u_;
@@ -25,27 +25,27 @@ contract LiquidTest is BaseTest {
         beck = newUser("beck");
         u_ = owen.newToken("U", 1e9);
         v_ = owen.newToken("V", 1e9);
-        U = one.make(u_);
-        V = one.make(v_);
+        U = WATER.make(u_);
+        V = WATER.make(v_);
 
-        address spring = one.WATER_UTILITY();
+        address spring = WATER.WATER_UTILITY();
         console.log("spring:", spring);
-        uint256 all = one.balanceOf(spring);
+        uint256 all = WATER.balanceOf(spring);
 
         vm.prank(spring);
         // forge-lint: disable-next-line(erc20-unchecked-transfer)
-        one.transfer(address(owen), all);
+        WATER.transfer(address(owen), all);
     }
 
     function newUser(string memory name) internal returns (LiquidUser u) {
-        u = new LiquidUser(name, one);
+        u = new LiquidUser(name, WATER);
     }
 
     function test_Pool() public returns (uint256 du, uint256 out) {
-        owen.give(address(alex), 1e3, one);
+        owen.give(address(alex), 1e3, WATER);
         owen.give(address(alex), 1e3, u_);
         owen.give(address(alex), 1e3, v_);
-        owen.give(address(beck), 1e7, one);
+        owen.give(address(beck), 1e7, WATER);
         owen.give(address(beck), 1e3, u_);
         owen.give(address(beck), 1e3, v_);
         owen.melt(U, 500);
@@ -60,11 +60,11 @@ contract LiquidTest is BaseTest {
         console.log("alex.liquidate:", U.symbol());
         console.log("alex.liquidate.du:", du);
         console.log("alex.liquidate.out:", out);
-        assertEq(1e3, alex.balance(one), "alex should have the same 1");
+        assertEq(1e3, alex.balance(WATER), "alex should have the same 1");
         (du, out) = beck.liquidate(U);
         console.log("beck.liquidate:", U.symbol());
         console.log("beck.liquidate.du:", du);
         console.log("beck.liquidate.out:", out);
-        assertEq(beck.balance(one), 1e7, "beck should have the same 1");
+        assertEq(beck.balance(WATER), 1e7, "beck should have the same 1");
     }
 }
