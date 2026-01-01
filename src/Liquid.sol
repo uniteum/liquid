6d2607b26b1d13fb88fe473e644e56c37876dee5
Paul Reinholdtsen <paul@bitsy.services>
2025-12-31 16:00:56 -0800

    heat cold stuff

--- src/Liquid.sol
+++ src/Liquid.sol
@@ -20,6 +20,12 @@ contract Liquid is ERC20, ReentrancyGuardTransient {
         solidOf[address(this)] = ice;
     }
 
+    function heat(uint256 cold, IERC20Metadata stuff) external {
+        Liquid liquid = heat(stuff);
+        liquid.heat(cold);
+        IERC20Metadata(liquid).safeTransfer(msg.sender, cold);
+    }
+
     function heat(uint256 cold) external nonReentrant {
         solid.safeTransferFrom(msg.sender, address(this), cold);
         _mint(address(this), cold);
