dbec6f8ec771ed7f85b022f41f2df4dc9da2edb6
Paul Reinholdtsen <paul@bitsy.services>
2026-01-02 14:14:47 -0800

    boil

--- src/Mob.sol
+++ src/Mob.sol
@@ -13,23 +13,23 @@ contract Mob {
     error BadMessage();
     error CallFailed(bytes32 h);
 
-    event Make(Mob mob, address[] members, uint256[] rage_, uint256 riot);
+    event Make(Mob mob, address[] members, uint256[] rage_, uint256 boil);
 
     Mob public immutable MOB = this;
 
     mapping(address => uint256) public rage;
-    uint256 public riot;
+    uint256 public boil;
 
     mapping(bytes32 => uint256) public fury;
     mapping(bytes32 => mapping(address => bool)) public yelled;
-    mapping(bytes32 => bool) public rioted;
+    mapping(bytes32 => bool) public boiled;
 
-    function made(address[] memory members, uint256[] memory rage_, uint256 riot_)
+    function made(address[] memory members, uint256[] memory rage_, uint256 boil_)
         public
         view
         returns (address location, bytes32 salt)
     {
-        if (riot_ == 0) {
+        if (boil_ == 0) {
             revert RiotAlways();
         }
         if (members.length != rage_.length) {
@@ -44,35 +44,35 @@ contract Mob {
             }
             sum += w;
         }
-        if (riot_ > sum) {
+        if (boil_ > sum) {
             revert RiotImpossible();
         }
-        salt = keccak256(abi.encode(members, rage_, riot_));
+        salt = keccak256(abi.encode(members, rage_, boil_));
         location = Clones.predictDeterministicAddress(address(MOB), salt, address(MOB));
     }
 
-    function make(address[] memory members, uint256[] memory rage_, uint256 riot_) public returns (Mob mob) {
+    function make(address[] memory members, uint256[] memory rage_, uint256 boil_) public returns (Mob mob) {
         if (this != MOB) {
-            mob = MOB.make(members, rage_, riot_);
+            mob = MOB.make(members, rage_, boil_);
         } else {
-            (address location, bytes32 salt) = made(members, rage_, riot_);
+            (address location, bytes32 salt) = made(members, rage_, boil_);
             mob = Mob(payable(location));
             if (location.code.length == 0) {
                 location = Clones.cloneDeterministic(address(MOB), salt);
-                mob.__initialize(members, rage_, riot_);
-                emit Make(mob, members, rage_, riot_);
+                mob.__initialize(members, rage_, boil_);
+                emit Make(mob, members, rage_, boil_);
             }
         }
     }
 
-    function __initialize(address[] memory members, uint256[] memory rage_, uint256 riot_) external {
-        if (riot != 0) {
+    function __initialize(address[] memory members, uint256[] memory rage_, uint256 boil_) external {
+        if (boil != 0) {
             revert AlreadyInitialized();
         }
         for (uint256 i = 0; i < members.length; i++) {
             rage[members[i]] = rage_[i];
         }
-        riot = riot_;
+        boil = boil_;
     }
 
     receive() external payable {}
@@ -86,7 +86,7 @@ contract Mob {
 
         bytes32 h = keccak256(yell);
 
-        if (rioted[h]) revert AlreadyRioted(h);
+        if (boiled[h]) revert AlreadyRioted(h);
         uint256 total = fury[h];
         if (!yelled[h][msg.sender]) {
             yelled[h][msg.sender] = true;
@@ -94,12 +94,12 @@ contract Mob {
             fury[h] = total;
         }
 
-        if (total >= riot) {
-            _riot(h, yell);
+        if (total >= boil) {
+            _boil(h, yell);
         }
     }
 
-    function _riot(bytes32 h, bytes calldata yell) private {
+    function _boil(bytes32 h, bytes calldata yell) private {
         address to;
         uint256 value;
 
@@ -113,6 +113,6 @@ contract Mob {
         (bool ok,) = to.call{value: value}(data);
         if (!ok) revert CallFailed(h);
 
-        rioted[h] = true;
+        boiled[h] = true;
     }
 }
