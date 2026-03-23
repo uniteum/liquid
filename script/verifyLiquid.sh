4a776951f194cf883e295fcd4ee5dca20c6f0f49
Paul Reinholdtsen <paul@bitsy.services>
2026-03-23 06:39:43 -0500

    Deploy to Ethereum mainnet chain=1

--- script/verifyLiquid.sh
+++ script/verifyLiquid.sh
@@ -1,3 +1,3 @@
-contract=$(jq -r '.transactions[0].contractAddress' broadcast/Solid.s.sol/$chain/run-latest.json)
-args=$(cast abi-encode "constructor(uint256)" $(jq -r '.transactions[].arguments[0]' broadcast/Solid.s.sol/$chain/run-latest.json))
-forge verify-contract $contract Solid --chain $chain --verifier etherscan --show-standard-json-input > script/Solid.json
+contract=$(jq -r '.transactions[0].contractAddress' broadcast/Liquid.s.sol/$chain/run-latest.json)
+args=$(cast abi-encode "constructor(address)" $(jq -r '.transactions[].arguments[0]' broadcast/Liquid.s.sol/$chain/run-latest.json))
+forge verify-contract $contract Liquid --chain $chain --verifier etherscan --show-standard-json-input > script/Liquid.json
